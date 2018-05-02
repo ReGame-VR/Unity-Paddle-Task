6461139a5454d0b9ec52090adda1dd76f94465a6
mtsandmeyer <32112441+mtsandmeyer@users.noreply.github.com>
2018-05-02 10:24:54 -0400

    Physics Tweaking

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,7 +6,7 @@ public class Ball : MonoBehaviour {
 
     // The force at which the ball will bounce upon collisions
     [SerializeField]
-    private float bounceForce = 0.26f;
+    private float bounceForce = 0.31f;
 
     // A reference to this ball's rigidbody
     private Rigidbody rigidBody;
@@ -21,23 +21,41 @@ public class Ball : MonoBehaviour {
         // On collision with paddle, ball should bounce
         if (c.gameObject.tag == "Paddle")
         {
+            BounceBall(c);
+        }
+    }
 
-            // Get velocity of paddle
-            float paddleVelocity = c.gameObject.GetComponent<VelocityNoRigidBody>().GetVelocity().magnitude;
-            
-            // Create a bounce velocity based on collision with paddle. Also include paddle velocity
-            // so that this bounce is proportional to movement of paddle
-            Vector3 bounceVelocity = c.contacts[0].normal * bounceForce * paddleVelocity;
+    void OnCollisionStay(Collision c)
+    {
+        if (c.gameObject.tag == "Paddle")
+        {
+            BounceBall(c);
+        }
+    }
 
-            // reset the velocity of the ball
-            rigidBody.velocity = new Vector3(0, 0, 0);
-           
-            // Exert the new velocity on the ball
-            rigidBody.AddForce(bounceVelocity, ForceMode.Impulse);
+    private void BounceBall(Collision c)
+    {
+        // Get velocity of paddle
+        float paddleVelocity = c.gameObject.GetComponent<VelocityNoRigidBody>().GetVelocity().magnitude;
+
+        // Create a bounce velocity based on collision with paddle. Also include paddle velocity
+        // so that this bounce is proportional to movement of paddle
+
+        Vector3 bounceVelocity;
+
+        if (paddleVelocity < 0.3)
+        {
+            bounceVelocity = c.contacts[0].normal * bounceForce * 0.3f;
         }
         else
         {
-            // On collision with everything else, ball should behave normally (roll, not bounce)
+            bounceVelocity = c.contacts[0].normal * bounceForce * paddleVelocity;
         }
+
+        // reset the velocity of the ball
+        rigidBody.velocity = new Vector3(0, 0, 0);
+
+        // Exert the new velocity on the ball
+        rigidBody.AddForce(bounceVelocity, ForceMode.Impulse);
     }
 }

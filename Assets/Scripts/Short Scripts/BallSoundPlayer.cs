75e48fa89fcf4c61f599cff06ad975a1b3b0de0f
Alan Zhou <30447373+alanxyzhou@users.noreply.github.com>
2019-04-23 18:58:27 -0400

    Replace success particles with sound effect
    - Extend drop sound player to include success sound
    - Remove apex particle spawner

--- Assets/Scripts/Short Scripts/BallSoundPlayer.cs
+++ Assets/Scripts/Short Scripts/BallSoundPlayer.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DropSoundPlayer : MonoBehaviour {
-
-    // The bounce sound effects. Pick randomly from this array
+public class BallSoundPlayer : MonoBehaviour {
+    
     public AudioSource dropSound;
+    public AudioSource successSound;
 
     // Play high pitch sound when ball is about to drop.
     public void PlayDropSound()
@@ -13,6 +13,13 @@ public class DropSoundPlayer : MonoBehaviour {
         // Stop the bounce sounds that are currently playing. We dont want to overload the audio.
         dropSound.Stop();
         dropSound.PlayOneShot(dropSound.clip);  
+    }
+
+    // Play success sound when ball apex is within target window
+    public void PlaySuccessSound()
+    {
+        successSound.Stop();
+        successSound.PlayOneShot(successSound.clip);
     }
     
 }

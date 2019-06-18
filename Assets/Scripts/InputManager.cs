1d89df576cef54112ed13f437baf2a62f4a6fc38
alanxyzhou <32112441+alanxyzhou@users.noreply.github.com>
2019-06-18 19:06:42 -0400

    Added Keyboard Input Manager
    - Consolidated keyboard input listeners to unified InputManager
    - Added ability to re-calibrate Target Line height to HMD height by pressing "c"
    - Improved debugger overlay
    - Set default ball respawn time to 1s for development purposes

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// InputManager listens for keyboard input and calls the appropriate function.
+public class InputManager : MonoBehaviour
+{  
+    void Update()
+    {
+        ListenForInput();
+    }
+
+    private void ListenForInput()
+    {
+        // Toggle pause/resume state
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject.Find("[SteamVR]").GetComponent<GlobalPauseHandler>().TogglePause();
+        }
+
+        // Toggle debugger overlay
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GameObject.Find("Debugger Display").GetComponent<DebuggerDisplay>().ToggleDisplay();
+        }
+
+        // Re-calibrate target line height
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GameObject.Find("[SteamVR]").GetComponent<PaddleGame>().SetTargetLineHeight();
+        }
+    }
+}
